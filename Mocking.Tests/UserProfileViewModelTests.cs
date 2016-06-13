5810f0befac981e34516c243e21a6c8671d540f1
Axel Pötzsch <poetzsch@webit.de>
2016-06-13 14:16:48 +0200

    [FEATURE] add missing membership mocking tests

--- Mocking.Tests/UserProfileViewModelTests.cs
+++ Mocking.Tests/UserProfileViewModelTests.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
 using NSubstitute.ReturnsExtensions;
 using Sitecore.FakeDb.Security.Web;
+using Sitecore.Globalization;
 
 namespace Mocking.Tests
 {
@@ -16,40 +17,65 @@ namespace Mocking.Tests
     {
       using (new MembershipSwitcher(MembershipProviderSetup()))
       {
-        var viewModel = new UserProfileViewModel(_userName);
+        var profileViewModel = new UserProfileViewModel(_userName);
 
-        Assert.IsTrue(viewModel.IsValidUser);
+        Assert.IsTrue(profileViewModel.IsValidUser);
       }
     }
 
     [TestMethod]
     public void ShouldBeInvalidUser()
     {
-      Assert.Fail();
+      using (new MembershipSwitcher(MembershipProviderSetup()))
+      {
+        var profileViewModel = new UserProfileViewModel(@"sitecore\INVALID.USER");
+
+        Assert.IsFalse(profileViewModel.IsValidUser);
+      }
     }
 
     [TestMethod]
     public void ShouldHaveTitle()
     {
-      Assert.Fail();
+      using (new MembershipSwitcher(MembershipProviderSetup()))
+      {
+        var profileViewModel = new UserProfileViewModel(_userName);
+
+        Assert.AreEqual("Dr.", profileViewModel.Title);
+      }
     }
 
     [TestMethod]
     public void ShouldHaveDefaultMessageForTitle()
     {
-      Assert.Fail();
+      using (new MembershipSwitcher(MembershipProviderSetup()))
+      {
+        var profileViewModel = new UserProfileViewModel(@"sitecore\INVALID.USER");
+
+        Assert.AreEqual(Translate.Text("UserNotExisting"), profileViewModel.Title);
+      }
     }
 
     [TestMethod]
     public void ShouldHaveDisplayName()
     {
-      Assert.Fail();
+      using (new MembershipSwitcher(MembershipProviderSetup()))
+      {
+        var profileViewModel = new UserProfileViewModel(_userName);
+
+        Assert.AreEqual("User, Test", profileViewModel.DisplayName);
+      }
     }
 
     [TestMethod]
     public void ShouldHaveDefaultMessageForDisplayName()
     {
-      Assert.Fail();
+      using (new MembershipSwitcher(MembershipProviderSetup()))
+      {
+        var profileViewModel = new UserProfileViewModel(@"sitecore\INVALID.USER");
+
+        Assert.AreEqual(Translate.Text("UserNotExisting"), profileViewModel.DisplayName);
+      }
     }
     
 
@@ -66,7 +92,7 @@ namespace Mocking.Tests
       provider.GetUser(Arg.Is(_userName), Arg.Any<bool>()).Returns(user);
 
       // use provider and set some user details (will be stored in a static var in 
-      // Webit.Jeno.Sc.Data.Intranet.Test.TestSupport.TestProfileProvider - see App.config)
+      // Mocking.Tests.FakeDbExtensions.TestProfileProvider - see App.config)
       using (new MembershipSwitcher(provider))
       {
         var scUser = Sitecore.Security.Accounts.User.FromName(_userName, true);
